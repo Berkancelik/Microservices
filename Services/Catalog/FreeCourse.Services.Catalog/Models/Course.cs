3da138b5970fe9357c06b1424be38420d41a258e
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-03 00:06:01 +0300

    some additions have been made

--- Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -11,12 +11,19 @@ namespace FreeCourse.Services.Catalog.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         [BsonRepresentation(BsonType.Decimal128)]
         public decimal Price { get; set; }
         public string UserId { get; set; }
         public string Picture { get; set; }
         [BsonRepresentation(BsonType.DateTime)]
         public DateTime CreatedTime { get; set; }
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
 
     }
 }
