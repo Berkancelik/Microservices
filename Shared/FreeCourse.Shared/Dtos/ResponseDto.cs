d87aa4a4582cbf9b6d6a4dbd35485e31b7a24acc
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-05 16:07:45 +0300

    ResponseDto created

---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class ResponseDto<T>
+    { //Static Factory Metot
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessFull { get; private set; }
+        public List<string> Errors { get; set; }
+
+        public static ResponseDto<T> Succes(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccessFull = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default(T), StatusCode = statusCode, IsSuccessFull = true };
+        }
+
+        public static ResponseDto<T> Fail(List<string> errors, int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccessFull = false
+            };
+        }
+
+        public static ResponseDto<T> Fail(string error, int statusCode)
+        {
+            return new ResponseDto<T> { Errors = new List<string> { error }, StatusCode = statusCode, IsSuccessFull = false };
+        }
+    }
+}
