811273ab2335ad398b51e85567829abfbedc8a64
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-05 16:27:52 +0300

    AutoMapper nuget package added

--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
@@ -6,7 +6,7 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string UserId { get; set; }
-        public string Picture { get set; }
+        public string Picture { get; set; }
         public FeatureDto Feature { get; set; }
         public string CategoryId { get; set; }
     }
