26ae297025323c1203c65542ef55cdf14cbddb25
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-06 14:05:08 +0300

    ICategoryService hsa been created

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Catalog.Services
 {
-    public class CategoryService
+    public class CategoryService:ICategoryService
     {
         private readonly IMongoCollection<Category> _categoryCollection;
         private readonly IMapper _mapper;
