2ef9351c7c0af13770b4cd74948fc8a3cbf57d00
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-06 14:16:55 +0300

    Refactoring

--- Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -19,7 +19,6 @@ namespace FreeCourse.Services.Catalog
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ICategoryService, CategoryService>();
@@ -40,7 +39,6 @@ namespace FreeCourse.Services.Catalog
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
