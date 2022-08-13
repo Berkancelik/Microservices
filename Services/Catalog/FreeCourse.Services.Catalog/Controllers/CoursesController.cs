53944cb063da63636d1fc53ec2f8f3ec6d5eb3c0
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-13 14:31:59 +0300

    CourseController and CustomBaseController created

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+    }
+}
