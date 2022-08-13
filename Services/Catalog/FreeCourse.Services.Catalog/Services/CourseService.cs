a22f7e298a4931d5efaacb726cab9581b1e25212
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-13 13:38:08 +0300

    Some fixes

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    internal class CourseService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CourseService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _categoryCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _mapper = mapper;
+        }
+    }
+}
